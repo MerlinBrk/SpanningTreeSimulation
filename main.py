a82af9362ceeee9de9a123cdfe48335d497d0a90
MerlinBrk <merlin.buerkle@gmail.com>
2025-09-09 08:10:26 +0200

    removed firste class Node

--- main.py
+++ main.py
@@ -1,43 +1,9 @@
 class Node:
-    def __init__(self, root_id, id, cost=0):
-        self.root_id = root_id
-        self.id = id
-        self.cost = cost
-        self.neighbors = []  
-        self.received_messages = []  
-
-    def add_neighbor(self, neighbor_node, weight):
-        self.neighbors.append((neighbor_node, weight))
-
-    def send_message(self):
-        for neighbor, weight in self.neighbors:
-            neighbor.receive_message(self.root_id, self.cost, weight)
-
-    def receive_message(self, root_id, cost, weight_to_neighbor):
-        self.received_messages.append((root_id, cost, weight_to_neighbor))
-
-    def update_state(self):
-        updated = False
-        for root_id, cost, weight_to_neighbor in self.received_messages:
-            total_cost = cost + weight_to_neighbor
-
-            if root_id < self.root_id:
-                self.root_id = root_id
-                self.cost = total_cost
-                updated = True
-            elif root_id == self.root_id and total_cost < self.cost:
-                self.cost = total_cost
-                updated = True
-
-        self.received_messages.clear()
-        return updated
-    
-class Node:
     def __init__(self, id):
         self.id = id
-        self.root_id = id  # Start: jeder Knoten sieht sich selbst als Root
+        self.root_id = id  
         self.cost = 0
-        self.neighbors = []  # list of (neighbor_node, weight)
+        self.neighbors = []  
         self.received_messages = []
 
     def add_neighbor(self, neighbor_node, weight):
