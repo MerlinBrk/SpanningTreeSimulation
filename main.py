1dac0995c3b04553dd4173589245e0b94376e160
MerlinBrk <merlin.buerkle@gmail.com>
2025-09-09 09:22:55 +0200

    added used neighbours and name

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
 class Node:
-    def __init__(self, id):
+    def __init__(self, id,name):
         self.id = id
         self.root_id = id  
+        self.name = name
         self.cost = 0
         self.neighbors = []  
         self.received_messages = []
+        self.used_neighbors = None
 
     def add_neighbor(self, neighbor_node, weight):
         self.neighbors.append((neighbor_node, weight))
 
     def send_message(self):
         for neighbor, weight in self.neighbors:
-            neighbor.receive_message(self.root_id, self.cost, weight)
+            neighbor.receive_message(self.root_id, self.cost, weight, self.id)
 
-    def receive_message(self, root_id, cost, weight_to_neighbor):
-        self.received_messages.append((root_id, cost, weight_to_neighbor))
+    def receive_message(self, root_id, cost, weight_to_neighbor, neighbor_id):
+        self.received_messages.append((root_id, cost, weight_to_neighbor, neighbor_id))
 
     def update_state(self):
         updated = False
-        for root_id, cost, weight_to_neighbor in self.received_messages:
+        for root_id, cost, weight_to_neighbor, neighbor_id in self.received_messages:
             total_cost = cost + weight_to_neighbor
             if root_id < self.root_id:
                 self.root_id = root_id
                 self.cost = total_cost
                 updated = True
+                self.add_used_neighbor(neighbor_id)
             elif root_id == self.root_id and total_cost < self.cost:
                 self.cost = total_cost
                 updated = True
+                self.add_used_neighbor(neighbor_id)
         self.received_messages.clear()
         return updated
+    
+    def add_used_neighbor(self, node_id):
+        for neighbor in self.neighbors:
+            if neighbor[0].id == node_id:
+                self.used_neighbors = neighbor[0]
+                break
+        
 
 
-A = Node(5)
-B = Node(1)
-C = Node(3)
-D = Node(7)
-E = Node(6)
-F = Node(4)
+A = Node(5,"A")
+B = Node(1,"B")
+C = Node(3,"C")
+D = Node(7,"D")
+E = Node(6,"E")
+F = Node(4,"F")
 
 
 A.add_neighbor(B, 10)
@@ -82,6 +93,7 @@ while not converged and rounds < all_nodes.__len__():
 
 
 for node in all_nodes:
-    print(f"Node {node.id}: Root={node.root_id}, Cost={node.cost}")
+    print(f"Node {node.name}: Root={node.root_id}, Cost={node.cost}")
+    print(f"Used neighbor: {node.used_neighbors.name if node.used_neighbors else 'None'}")
 
 
